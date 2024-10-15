64955b5b6dd11b755a35be91c6c2e3892004d30a
Vinícius Massari <stonefields11@hotmail.com>
2024-10-15 12:16:53 -0300

    feat: list all jobs by company use case

--- src/main/java/br/com/viniciusmassari/gestao_vagas/modules/company/controllers/JobsController.java
+++ src/main/java/br/com/viniciusmassari/gestao_vagas/modules/company/controllers/JobsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import br.com.viniciusmassari.gestao_vagas.exceptions.CompanyNotFoundException;
 import br.com.viniciusmassari.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.viniciusmassari.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.viniciusmassari.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import br.com.viniciusmassari.gestao_vagas.modules.company.useCases.ListAllJobsByCompanyUseCase;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -29,6 +31,9 @@ import jakarta.validation.Valid;
 @RequestMapping("/job")
 @Tag(description = "Métodos referentes a job", name = "Job Controller")
 public class JobsController {
+
+        @Autowired
+        ListAllJobsByCompanyUseCase listAllJobsByCompanyUseCase;
 
         @Autowired
         private CreateJobUseCase createJobUseCase;
@@ -63,5 +68,21 @@ public class JobsController {
                         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
 
                 }
+        }
+
+        @GetMapping("/")
+        @PreAuthorize("hasRole('COMPANY')")
+        @Tag(name = "Vagas", description = "Listagem das vagas")
+        @Operation(summary = "Listagem de vaga", description = "Essa função é responsável por listar as vagas da empresa")
+        @ApiResponses({
+                        @ApiResponse(responseCode = "200", content = {
+                                        @Content(schema = @Schema(implementation = JobEntity.class))
+                        })
+        })
+        @SecurityRequirement(name = "jwt_auth")
+        public ResponseEntity<Object> listByCompany(HttpServletRequest request) {
+                var companyId = request.getAttribute("company_id");
+                var result = this.listAllJobsByCompanyUseCase.execute(UUID.fromString(companyId.toString()));
+                return ResponseEntity.ok().body(result);
         }
 }
